b41ad5ab4004adae36beb02496a9db60931d8410
onrczz <onurcilhoroz@gmail.com>
2026-03-19 20:28:29 +0300

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,6 +1,6 @@
 const https = require('https');
 
-export default async function handler(req, res) {
+module.exports = async function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
